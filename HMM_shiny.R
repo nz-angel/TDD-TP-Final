24678c782ad608108bc087dc930ffa4fde092854
nz-angel <nafm@live.com.ar>
2020-07-13 17:02:21 -0300

    Shiny para Modelo Oculto Markoviano

--- HMM_shiny.R
+++ HMM_shiny.R
@@ -3,8 +3,11 @@ library(ggplot2)
 library(gridExtra)
 
 recursive_HMM = function(N, noise, din){
+  # Esta funcion generaliza la implementada en HMM.R : permite elegir cuantos
+  # intervalos temporales se van a considerar, cu谩nto es el ruido de la
+  # percepcion y cual es la dinamica del estado del mundo.
   
-  # Se define la din醡ica del sistema
+  # Se define la din?mica del sistema
   M = matrix(c(1-din, din, din, 1-din), nrow = 2, ncol = 2)
   
   # Se simulan las observaciones
@@ -52,48 +55,53 @@ recursive_HMM = function(N, noise, din){
   
   ls = list(observations, mat_prev_post, mat_prior, mat_likelihood, mat_posterior)
   names(ls) = c('observations', 'prev_post', 'prior', 'like', 'post')
+  
   return(ls)
 }
 
 ui = fluidPage(
   h2(style="text-align:center", 'Inferencia bayesiana recursiva: 
      modelo oculto de Markov'),
-  h4(style="text-align:center", 'Teor韆 de toma de decisiones - 
+  h4(style="text-align:center", 'Teor铆a de toma de decisiones - 
      Primer cuatrimestre 2020 - Nazareno Faillace'),
   
   sidebarLayout(
     
     sidebarPanel(
-      p('Con esta aplicaci髇 se simula un ejemplo de inferencia
-             bayesiana recursiva cuando el estado del mundo es din醡ico y binario.
-             El experimento consiste en observar un animal a lo lejos y determinar
-             su direccion de movimiento (izquierda o derecha).'),
-      p('Como suele ocurrir, la percepci髇 del sujeto puede introducir ruido, 
-        por lo que hay cierta probabilidad', em('p_c'), 'de que el individuo 
-        observe la direcci髇 err髇ea. Por otro lado, el sujeto conoce la din醡ica: 
-        entre cada intervalo de tiempo: sabe que hay una probabilidad', 
-        em('p_d'), 'de que el animal cambie 
-        de direcci髇. Por eso, en cada intervalo de tiempo ajusta con esta
-        informaci髇 su creencia posterior en en el intervalo de tiempo previo
-        para dar lugar a su creencia a priori actual.'),
-      p('A continuaci髇, se pueden ajustar los valores de p_c y de p_d. Se puede
-        observar una animaci髇 '),
+      p(style="text-align:justify",'Con esta aplicaci贸n se simula un ejemplo de 
+      inferencia bayesiana recursiva cuando el estado del mundo es din谩mico y 
+      binario. El experimento consiste en observar un animal a lo lejos y 
+      determinar su direcci贸n de movimiento (izquierda o derecha).'),
+      p(style="text-align:justify", 'Como suele ocurrir, la percepci贸n del 
+      sujeto puede introducir ruido, por lo que hay cierta probabilidad', 
+      em(HTML('p<sub>c</sub>')), 'de que el individuo observe la direcci贸n 
+      err贸nea. Por otro lado, el sujeto conoce la din谩mica: entre cada intervalo
+      de tiempo: sabe que hay una probabilidad',  em(HTML('p<sub>d</sub>')), 
+      'de que el animal cambie de direcci贸n. Por eso, en cada intervalo de 
+      tiempo ajusta con esta informaci贸n su creencia posterior en en el 
+      intervalo de tiempo previo para dar lugar a su creencia a priori actual.'),
+      p(style="text-align:justify", 'A continuacion, se pueden ajustar los 
+        valores de', em(HTML('p<sub>c</sub>')),'y de', em(HTML('p<sub>d</sub>')),
+        '. Se puede observar, manualmente o con una animaci贸n, c贸mo evolucionan 
+        en el tiempo las creencias del sujeto. En la parte superior figura lo 
+        que observa en cada intervalo de tiempo.'),
       
       
       sliderInput('tsteps', 'Cantidad de espacios temporales', 
                   value=6,min = 2, max = 30, step = 1),
       
-      sliderInput('noise', 'Ruido en la percepcion', 
+      sliderInput('noise', HTML('Ruido en la percepcion (p<sub>c</sub>)'), 
                   value=0.25, min = 0.05, max=0.95, step=0.05),
       
-      numericInput('pancake', 'Probabilidad de que el animal cambie de direccion',
+      numericInput('pancake', HTML('Probabilidad de que el animal cambie de 
+                                   direccion (p<sub>d</sub>)'),
                    value=0.2, min=0, max=1, step=0.05),
       
       uiOutput('time')
     ),
     mainPanel(
       htmlOutput('obs'),
-      plotOutput('plots'),
+      plotOutput('plots', height="auto"),
     )
   ),
   
@@ -161,31 +169,12 @@ server = function(input, output){
     df = data.frame(direccion=c('I', 'D'), p=posterior)
     title = paste('Posterior a tiempo', input$time2)
     post_plt = ggplot(data=df, aes(x=direccion, y=p)) +
-      geom_bar(stat="identity", fill="mediumpurple")+
+      geom_bar(stat="identity", fill="tomato")+
       theme_minimal() + ylim(0,1) + ggtitle(title) +
       theme(plot.title = element_text(hjust = 0.5, size = 22))
     
     grid.arrange(prev_plt, prior_plt, like_plt, post_plt, ncol=2, nrow=2)
-    
-    # plot(x, prev_post, type='h', xlim=c(-0.5, 1.5), ylim=c(0,1), lwd=3, xaxt='n', main=title, xlab='', ylab='p')
-    # axis(1, at=c(0,1), labels=c('I', 'D'))
-    # points(x,prev_post,pch=16,cex=2.5,col="red")
-    # 
-    # title = paste('Prior a tiempo', input$time2)
-    # plot(x, prior, type='h', xlim=c(-0.5, 1.5), ylim=c(0,1), lwd=3, xaxt='n', main=title, xlab='', ylab='p')
-    # axis(1, at=c(0,1), labels=c('I', 'D'))
-    # points(x,prior,pch=16,cex=2.5,col="red")
-    # 
-    # title = paste('Likelihood a tiempo', input$time2)
-    # plot(x, likelihood, type='h', xlim=c(-0.5, 1.5), ylim=c(0,1), lwd=3, xaxt='n', main=title, xlab='', ylab='p')
-    # axis(1, at=c(0,1), labels=c('I', 'D'))
-    # points(x,likelihood,pch=16,cex=2.5,col="red")
-    # 
-    # title = paste('Posterior a tiempo', input$time2)
-    # plot(x, posterior, type='h', xlim=c(-0.5, 1.5), ylim=c(0,1), lwd=3, xaxt='n', main=title, xlab='', ylab='p')
-    # axis(1, at=c(0,1), labels=c('I', 'D'))
-    # points(x,posterior,pch=16,cex=2.5,col="red")
-    
+
     }, height=800)
 
   
